19a569339b77fd67ecca2364dc0d3fe38a98a96a
Philippe Cheype <philippe.cheype@epitech.eu>
2024-02-03 13:52:44 +0100

    First commit(): Added main architecture

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development', // TODO: 'production'
+  entry: {
+    background: './src/background/index.ts',
+    content_script: './src/content_script/index.ts'
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  devtool: 'inline-source-map',
+  watch: true
+};
